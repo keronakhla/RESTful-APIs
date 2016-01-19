fbdb2d0394d16b92dc8809aa76bcb7e00e7920ea
Kerolos <keronakhla@gmail.com>
2016-01-18 20:38:22 -0800

    Create Project

---
+++ Jaci/Jaci/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jaci
+//
+//  Created by Kerolos Nakhla on 1/18/16.
+//  Copyright © 2016 Kerolos Nakhla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jaci/Jaci/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jaci
+//
+//  Created by Kerolos Nakhla on 1/18/16.
+//  Copyright © 2016 Kerolos Nakhla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
